5d56e1d6536e1855c434a0e9051ddf5b885f19ae
yl <yl99@163.com>
2020-10-20 20:02:49 +0800

    g2o

--- Thirdparty/g2o/g2o/stuff/string_tools.cpp
+++ Thirdparty/g2o/g2o/stuff/string_tools.cpp
@@ -92,6 +92,20 @@ std::string strToUpper(const std::string& s)
   return ret;
 }
 
+#ifdef _WINDOWS
+int vasprintf(char** ptr, const char* format, va_list ap)
+{
+    int len;
+    len = _vscprintf_p(format, ap) + 1;
+    *ptr = (char*)malloc(len * sizeof(char));
+    if (!*ptr)
+    {
+        return -1;
+    }
+    return _vsprintf_p(*ptr, len, format, ap);
+}
+#endif
+
 std::string formatString(const char* fmt, ...)
 {
   char* auxPtr = NULL;
